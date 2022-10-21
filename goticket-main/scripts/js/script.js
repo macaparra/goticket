b065f898481409929045eb802ef0cf6cf2cc4392
gioannyrlr <gioannylop@hotmail.com>
2022-10-21 00:55:45 -0400

    creada pantalla registro
    faltan las validaciones

--- goticket-main/scripts/js/script.js
+++ goticket-main/scripts/js/script.js
@@ -1,3 +1,5 @@
+/* Inicio */
+
 const splash = document.querySelector('.splash');
 
 document.addEventListener('DOMContentLoaded', (e)=>{
@@ -5,3 +7,23 @@ document.addEventListener('DOMContentLoaded', (e)=>{
         splash.classList.add('display-none');
     }, 2000);
 })
+
+/* Registro */
+
+const form = document.querySelector("form"),
+        nextBtn = form.querySelector(".nextBtn"),
+        backBtn = form.querySelector(".backBtn"),
+        allInput = form.querySelectorAll(".first input");
+
+
+nextBtn.addEventListener("click", ()=> {
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
+        }
+    })
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
